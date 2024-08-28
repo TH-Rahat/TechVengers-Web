a955eb06b24146ed597ce6ded63f6cd064ced4fa
TH-Rahat <1foxgameing@gmail.com>
2024-08-29 02:04:08 +0600

    upgrade

--- index.js
+++ index.js
@@ -54,15 +54,3 @@ document.addEventListener("DOMContentLoaded", function () {
         observer.observe(counter);
     });
 });
-
-
-// career form
-// document.querySelector("form").addEventListener("submit", function(event) {
-//     let name = document.getElementById("name").value;
-//     let email = document.getElementById("email").value;
-
-//     if (name === "" || email === "") {
-//         alert("Name and email are required fields.");
-//         event.preventDefault();
-//     }
-// });
